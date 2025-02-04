921d03f42490c1e5f74a4089ec01072acf36ea79
Abhisek Das <dasabhisek668@gmail.com>
2025-02-04 23:10:40 +0530

    added darkmode storage

--- Weather App/assets/js/darkmode.js
+++ Weather App/assets/js/darkmode.js
@@ -1,22 +1,46 @@
-//get the system color profile
+//store the color profile in a local storage to keep the color profile when the user refreshes the page
+const colorProfile = localStorage.getItem('color-profile');
 
-const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-if(darkMode){
+if (colorProfile === 'dark') {
     document.body.classList.remove('lightMode');
-    console.log(darkMode);
 }
-else{
+else if (colorProfile === 'light') {
     document.body.classList.add('lightMode');
 }
+else {
 
 
-const lightmodeBtn = document.querySelector('.light-mode');
-const darkModeBtn = document.querySelector(".darkmode-btn");
+    //get the system color profile
+
+    const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (darkMode) {
+        document.body.classList.remove('lightMode');
+        // console.log(darkMode);
+    }
+    else {
+        document.body.classList.add('lightMode');
+    }
 
 
-const modeSwitch = document.querySelector(".darkmode-container");
+    // const lightmodeBtn = document.querySelector('.light-mode');
+    // const darkModeBtn = document.querySelector(".darkmode-btn");
 
-modeSwitch.addEventListener("click" , function(){
-    document.body.classList.toggle('lightMode');
-})
+
+    const modeSwitch = document.querySelector(".darkmode-container");
+
+    modeSwitch.addEventListener ("click", function () {
+        document.body.classList.toggle('lightMode');
+
+        if(document.body.classList.contains('lightMode'))
+        {
+            localStorage.setItem('color-profile' , 'light' );
+        }
+        else
+        {
+            localStorage.setItem('color-profile' , 'dark' );
+        }
+
+    })
+
+}
